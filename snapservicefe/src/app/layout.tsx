a6c46ac1b17c177b61d62e8639a7d415700f4776
sonlqt <sonlqtse184097@fpt.edu.vn>
2025-05-20 22:27:02 +0700

    Init project

---
+++ snapservicefe/src/app/layout.tsx
@@ -0,0 +1,24 @@
+// src/app/layout.tsx
+import './globals.css'
+import { ReactNode } from 'react'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
+export const metadata = {
+  title: 'SnapService',
+  description: 'SnapService ne',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="bg-white text-gray-800">
+        <Header />
+        <main className="min-h-screen container mx-auto p-4">
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
