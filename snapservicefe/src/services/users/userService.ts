9fda2be9cb669c519bb1188e0a14ec310f78a034
sonlqt <sonlqt.work@gmail.com>
2025-06-11 08:48:02 +0700

    remove any type

--- snapservicefe/src/services/users/userService.ts
+++ snapservicefe/src/services/users/userService.ts
@@ -1,36 +1,36 @@
 import { getAPI } from "@/lib/axios"; 
 
-const registerUser = async (data: any) => {
-  try {
-    const api = getAPI(); 
-    const res = await api.post("/api/Auth/register", data);
-    return res.data.data;
-  } catch (error: any) {
-    console.error("Registration failed:", error.response?.data || error.message);
-    throw error;
-  }
-};
+// const registerUser = async (data: any) => {
+//   try {
+//     const api = getAPI(); 
+//     const res = await api.post("/api/Auth/register", data);
+//     return res.data.data;
+//   } catch (error: any) {
+//     console.error("Registration failed:", error.response?.data || error.message);
+//     throw error;
+//   }
+// };
 
-const loginUser = async (data: any) => {
-  try {
-    const api = getAPI(); 
-    const res = await api.post("/api/Auth/login", data);
-    return res.data.data;
-  } catch (error: any) {
-    console.error("Login failed:", error.response?.data || error.message);
-    throw error;
-  }
-};
+// const loginUser = async (data: any) => {
+//   try {
+//     const api = getAPI(); 
+//     const res = await api.post("/api/Auth/login", data);
+//     return res.data.data;
+//   } catch (error: any) {
+//     console.error("Login failed:", error.response?.data || error.message);
+//     throw error;
+//   }
+// };
 
-export const getAllUsers = async () => {
-  try {
-    const api = getAPI();
-    const response = await api.get("/api/User/GetAllUser");
-    return response.data.data;
-  } catch (error: any) {
-    console.error("Failed to fetch users:", error.response?.data || error.message);
-    throw error;
-  }
-};
+// export const getAllUsers = async () => {
+//   try {
+//     const api = getAPI();
+//     const response = await api.get("/api/User/GetAllUser");
+//     return response.data.data;
+//   } catch (error: any) {
+//     console.error("Failed to fetch users:", error.response?.data || error.message);
+//     throw error;
+//   }
+// };
 
-export { registerUser, loginUser };
+// export { registerUser, loginUser };
