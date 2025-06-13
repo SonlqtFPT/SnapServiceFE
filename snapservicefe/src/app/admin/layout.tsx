307b6484758bb845bca6091b24c0c34d33c1cc4b
KimHang1504 <160225729+KimHang1504@users.noreply.github.com>
2025-06-13 22:43:36 +0700

    admin page

--- snapservicefe/src/app/admin/layout.tsx
+++ snapservicefe/src/app/admin/layout.tsx
@@ -1,14 +1,47 @@
-// import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-// import { AppSidebar } from "@/components/app-sidebar"
 
-// export default function Layout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <SidebarProvider>
-//       <AppSidebar />
-//       <main>
-//         <SidebarTrigger />
-//         {children}
-//       </main>
-//     </SidebarProvider>
-//   )
-// }
+import '../../app/globals.css';
+
+
+import { Sidebar } from "@/components/Layouts/sidebar";
+
+import "flatpickr/dist/flatpickr.min.css";
+import "jsvectormap/dist/jsvectormap.css";
+
+import { Header } from "@/components/Layouts/header";
+import type { Metadata } from "next";
+import NextTopLoader from "nextjs-toploader";
+import type { PropsWithChildren } from "react";
+import { Providers } from "./provides";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | NextAdmin - Next.js Dashboard Kit",
+    default: "NextAdmin - Next.js Dashboard Kit",
+  },
+  description:
+    "Next.js admin dashboard toolkit with 200+ templates, UI components, and integrations for fast dashboard development.",
+};
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <Providers>
+          <NextTopLoader color="#5750F1" showSpinner={false} />
+
+          <div className="flex min-h-screen">
+            <Sidebar />
+
+            <div className="w-full bg-gray-2 dark:bg-[#020d1a]">
+              <Header />
+
+              <main className="isolate mx-auto w-full max-w-screen-2xl overflow-hidden p-4 md:p-6 2xl:p-10">
+                {children}
+              </main>
+            </div>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
